356fc562c652a8382a4f62026e6a87d5cb0bf640
zemla4ok <dima-vital@live.com>
2017-11-27 22:20:47 +0300

    pizza description

--- models/pizza.js
+++ models/pizza.js
@@ -1,5 +1,22 @@
 module.exports = (Sequelize, sequelize) => {
     return sequelize.define('pizzas', {
         //todo: description of fields
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.STRING(500),
+            allowNull: false
+        },
+        description: {
+            type: Sequelize.STRING(500),
+            allowNull: false
+        },
+        calories: {
+            type: Sequelize.DOUBLE,
+            allowNull: false
+        }
     });
 };
