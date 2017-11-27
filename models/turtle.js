845edd28f70c507bde1e9e842d152725e3f93637
zemla4ok <dima-vital@live.com>
2017-11-27 22:12:56 +0300

    turtle model

--- models/turtle.js
+++ models/turtle.js
@@ -1,5 +1,27 @@
 module.exports = (Sequelize, sequelize) => {
     return sequelize.define('turtles', {
         //todo: description of fields
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.STRING(500),
+            allowNull: false
+        },
+        color: {
+            type: Sequelize.STRING(500),
+            allowNull: false
+        },
+        weaponId: {
+            type: Sequelize.INTEGER
+        },
+        firstFavouritePizzaId: {
+            type: Sequelize.INTEGER
+        },
+        secondFavouritePizzaId: {
+            type: Sequelize.INTEGER
+        }
     });
 };
