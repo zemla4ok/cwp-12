79dcdd385f0e28a38f1a059a8b2cd5fd4d54644f
zemla4ok <dima-vital@live.com>
2017-11-27 22:16:08 +0300

    weapon model

--- models/weapon.js
+++ models/weapon.js
@@ -1,5 +1,18 @@
 module.exports = (Sequelize, sequelize) => {
     return sequelize.define('weapons', {
         //todo: description of fields
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        name: {
+            type: Sequelize.STRING(500),
+            allowNull: false
+        },
+        dps: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        }
     });
 };
