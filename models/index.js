aeecbf1735a3e858e1338980cc993e73c2ad62bf
zemla4ok <dima-vital@live.com>
2017-11-27 22:50:56 +0300

    connecting to db

--- models/index.js
+++ models/index.js
@@ -1,9 +1,24 @@
 const Turtle = require('./turtle');
 const Weapon = require('./weapon');
 const Pizza = require('./pizza');
+const config = require('../config.json');
 
 module.exports = (Sequelize, config) => {
     //todo: create object for connecting to db-sequelize
+    const sequelize = new Sequelize(config.db,
+        config.login,
+        config.password,
+        {
+            host: config.host,
+            dialect: config.dialect,
+            logging: false
+        });
+    sequelize.authenticate()
+        .then(() => {
+            console.log('Success');
+        }).catch((err) => {
+            console.log('error in connecting', err);
+        })
 
     const turtles = Turtle(Sequelize, sequelize);
     const weapons = Weapon(Sequelize, sequelize);
